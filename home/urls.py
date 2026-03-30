9df34d50fa406968aba035ca6ec1f80bc8b421e9
ericdalaporta <ericdasilvadalaporta@gmail.com>
2026-03-30 13:40:40 -0300

    criando o front do admin com heranca de template do principal pros outros htmls

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import index
+from .views import index, reservas, emprestimos, registros, propriedades, chaves, notificacoes, usuarios, acessos
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('propriedades/', propriedades, name='propriedades'),
+    path('chaves/', chaves, name='chaves'),
+    path('usuarios/', usuarios, name='usuarios'),
+    path('acessos/', acessos, name='acessos'),
+    path('reservas/', reservas, name='reservas'),
+    path('emprestimos/', emprestimos, name='emprestimos'),
+    path('registros/', registros, name='registros'),
+    path('notificacoes/', notificacoes, name='notificacoes'),
 ]
