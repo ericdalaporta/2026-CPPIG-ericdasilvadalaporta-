11b8c1408d63bb104d040b050f417cfb8e84f19c
ericdalaporta <ericdasilvadalaporta@gmail.com>
2026-06-24 16:02:22 -0300

    removendo script automatico

--- iniciar_sistema.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-start "Django" cmd /k "call .venv\Scripts\activate.bat && python manage.py runserver"
-start "Scheduler" cmd /k "call .venv\Scripts\activate.bat && python manage.py run_scheduler"
